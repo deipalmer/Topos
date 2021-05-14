79eb0dfa6c5dffe2ef3e20d2151c44669ef4f25f
deipalmer <deipalmer99@gmail.com>
2021-05-14 16:35:47 +0100

    Sonidos agregados y codigo empezado

---
+++ TOPOS/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float tiempo;
+    public GameObject topo;
+    Topos[] topos;
+    public float salir = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        topos = topo.GetComponentsInChildren<Topos>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempo += Time.deltaTime;
+
+        if (tiempo <= 60)
+        {
+            salir = Random.Range(0, 100);
+            if (salir > 95f)
+            {
+                topos[Random.Range(0, topos.Length)].TopoVisible();
+                salir = 0f;
+            }
+        }
+    }
+}
---
+++ TOPOS/Assets/Scripts/Topos.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Topos : MonoBehaviour
+{
+    public float oculto;
+    public float visible;
+    Vector3 altura;
+    float velocidad = 3f;
+    public float tiempoVisible;
+
+    void Awake()
+    {
+        TopoOculto();
+        transform.localPosition = altura;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = Vector3.Lerp(transform.localPosition, altura, velocidad * Time.deltaTime);
+
+        tiempoVisible -= Time.deltaTime;
+
+        if (tiempoVisible < 0)
+        {
+            TopoOculto();
+        }
+    }
+
+    public void TopoOculto()
+    {
+        altura = new Vector3(transform.localPosition.x, oculto, transform.localPosition.z);
+    }
+
+    public void TopoVisible()
+    {
+        altura = new Vector3(transform.localPosition.x, visible, transform.localPosition.z);
+        tiempoVisible = 2f;
+    }
+}
